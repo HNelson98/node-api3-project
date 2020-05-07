bc3fa18bc67ce4f6130db282bee8f75d867149b0
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-07 15:42:02 -0700

    The user gets are working

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 const server = express();
 
+server.use(express.json());
+server.use(logger);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
+server.use('/api/users', userRouter)
+server.use('/api/posts', userRouter)
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`)
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`)
 
   next()
 }
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const Users = require('./userDb')
+const Posts = require('../posts/postDb')
 
 router.post('/', (req, res) => {
   // do your magic!
@@ -10,16 +12,44 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// GET all users
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      console.log('GET all users err', err)
+      res.status(500).json({ errorMessage: 'could not find users' })
+    })
 });
 
+
+// GET user by id
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = Number(req.params.id)
+
+
+  Users.getById(id)
+    .then(user => {
+      if (user.length > 0) {
+        res.status(200).json(user)
+      } else {
+        res.status(400).json({ message: "invalid user id" })
+      }
+    })
+    .catch(err => {
+      console.log('GET user by id err', err)
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const id = Number(req.params.id)
+
+
 });
 
 router.delete('/:id', (req, res) => {
